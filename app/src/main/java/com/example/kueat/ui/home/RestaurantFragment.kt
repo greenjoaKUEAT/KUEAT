bff7d20cc1067b07a13a9c812ddc63b756153d46
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-06-02 23:16:13 +0900

    add tablayout

--- app/src/main/java/com/example/kueat/ui/home/RestaurantFragment.kt
+++ app/src/main/java/com/example/kueat/ui/home/RestaurantFragment.kt
@@ -8,9 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.kueat.R
 import com.example.kueat.databinding.FragmentRestaurantBinding
+import com.google.android.material.tabs.TabLayout
 
 class RestaurantFragment : Fragment() {
     lateinit var binding:FragmentRestaurantBinding
+    lateinit var tab1 : TabLayout.Tab
+    lateinit var tab2 : TabLayout.Tab
+    lateinit var tab3 : TabLayout.Tab
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,6 +26,29 @@ class RestaurantFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        initTab()
+        binding.apply {
+
+        }
+
+    }
+
+    private fun initTab() {
+        binding.apply {
+            tab1 = tablayout.newTab()
+            tab2 = tablayout.newTab()
+            tab3 = tablayout.newTab()
+            tab1.text = "정보"
+            tab2.text = "메뉴"
+            tab3.text = "리뷰"
+            tablayout.addTab(tab1)
+            tablayout.addTab(tab2)
+            tablayout.addTab(tab3)
+        }
     }
 
 }
