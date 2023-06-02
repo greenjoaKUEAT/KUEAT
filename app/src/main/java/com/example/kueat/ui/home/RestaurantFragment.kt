939e457dbecb6ff284ea660f90391518d565fbe8
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-06-03 04:18:51 +0900

    tab 이동 구현

--- app/src/main/java/com/example/kueat/ui/home/RestaurantFragment.kt
+++ app/src/main/java/com/example/kueat/ui/home/RestaurantFragment.kt
@@ -1,16 +1,21 @@
 package com.example.kueat.ui.home
 
+import android.graphics.Rect
 import android.os.Binder
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
 import com.example.kueat.R
 import com.example.kueat.databinding.FragmentRestaurantBinding
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import kotlin.math.abs
 
-class RestaurantFragment : Fragment() {
+class RestaurantFragment : Fragment(),TabLayout.OnTabSelectedListener,View.OnScrollChangeListener{
     lateinit var binding:FragmentRestaurantBinding
     lateinit var tab1 : TabLayout.Tab
     lateinit var tab2 : TabLayout.Tab
@@ -32,7 +37,8 @@ class RestaurantFragment : Fragment() {
     private fun initLayout() {
         initTab()
         binding.apply {
-
+            tablayout.addOnTabSelectedListener(this@RestaurantFragment)
+            scrollView.setOnScrollChangeListener(this@RestaurantFragment)
         }
 
     }
@@ -51,4 +57,56 @@ class RestaurantFragment : Fragment() {
         }
     }
 
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+
+        when (tab?.position) {
+            0 -> {
+                binding.scrollView.scrollToView(binding.textRest)
+            }
+            1 -> {
+                binding.scrollView.scrollToView(binding.textMenu)
+            }
+            2 -> {
+                binding.scrollView.scrollToView(binding.textReview)
+            }
+        }
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+        //do nothing
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+        //do nothing
+    }
+
+    override fun onScrollChange(
+        v: View?,
+        scrollX: Int,
+        scrollY: Int,
+        oldScrollX: Int,
+        oldScrollY: Int
+    ) {
+       // scroll 이동 시 tab 전환 구현
+
+    }
+
+    fun ScrollView.computeDistanceToView(view: View): Int {
+        return abs(calculateRectOnScreen(this).top - (this.scrollY + calculateRectOnScreen(view).top))
+    }
+
+    fun calculateRectOnScreen(view: View): Rect {
+        val location = IntArray(2)
+        view.getLocationOnScreen(location)
+        return Rect(
+            location[0],
+            location[1],
+            location[0] + view.measuredWidth,
+            location[1] + view.measuredHeight
+        )
+    }
+    fun ScrollView.scrollToView(view: View) {
+        val y = computeDistanceToView(view)
+        this.scrollTo(0, y)
+    }
 }
