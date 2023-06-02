2f5b5d3ee3d8f321d5efe7ab2720af5c80dd959a
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-06-03 00:17:51 +0900

    tab layout 구성 완료

--- app/src/main/java/com/example/kueat/MainActivity.kt
+++ app/src/main/java/com/example/kueat/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.kueat.databinding.ActivityMainBinding
 import com.example.kueat.ui.account.AccountFragment
 import com.example.kueat.ui.appeal.AppealFragment
 import com.example.kueat.ui.home.HomeFragment
+import com.example.kueat.ui.home.RestaurantFragment
 import com.example.kueat.ui.like.LikeFragment
 import com.google.android.material.navigation.NavigationBarView
 
@@ -43,7 +44,7 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.navigation_like -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, LikeFragment()).commit()
-            R.id.navigation_home -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commit()
+            R.id.navigation_home -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, RestaurantFragment()).commit()
             R.id.navigation_appeal -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, AppealFragment()).commit()
             R.id.navigation_account -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, AccountFragment()).commit()
         }
