e088cd4f0975ed278c119e95d2ba921a9ac19e80
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-06-04 22:11:43 +0900

    pull develop

--- app/src/main/java/com/example/kueat/MainActivity.kt
+++ app/src/main/java/com/example/kueat/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.navigation_like -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, LikeFragment()).commit()
-            R.id.navigation_home -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, RestaurantFragment()).commit()
+            R.id.navigation_home -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commit()
             R.id.navigation_appeal -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, AppealFragment()).commit()
             R.id.navigation_account -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, AccountFragment()).commit()
         }
