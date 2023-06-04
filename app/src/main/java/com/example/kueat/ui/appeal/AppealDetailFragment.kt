6de697555c78893ed47775e8058d3d276bbe70bc
nonaninona <starcraft0529@gmail.com>
2023-06-04 22:23:53 +0900

    #8 청원화면 구성 브랜치 생성 및 기본 xml 작성

--- app/src/main/java/com/example/kueat/ui/appeal/AppealDetailFragment.kt
+++ app/src/main/java/com/example/kueat/ui/appeal/AppealDetailFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.kueat.databinding.FragmentAppealDetailBinding
+import com.example.kueat.databinding.FragmentAppealBinding
 
 class AppealDetailFragment : Fragment() {
 
-    lateinit var binding: FragmentAppealDetailBinding
+    lateinit var binding: FragmentAppealBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentAppealDetailBinding.inflate(inflater, container, false)
+        binding = FragmentAppealBinding.inflate(inflater, container, false)
 
         return binding.root
     }
