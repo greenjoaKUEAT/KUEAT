d103006d6ab9a6db014868937a8bbbf2a9f8795a
DESKTOP-BO4OKLN\user <jinwoo0302@naver.com>
2023-06-08 20:06:59 +0900

    필터 프래그먼트에서 선택한 값 홈 프래그먼트로 전달, 레이아웃 완성.

--- app/src/main/java/com/example/kueat/MainActivity.kt
+++ app/src/main/java/com/example/kueat/MainActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils.replace
 import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.kueat.databinding.ActivityMainBinding
 import com.example.kueat.ui.account.AccountFragment
 import com.example.kueat.ui.appeal.AppealFragment
+import com.example.kueat.ui.filter.FilterMenuFragment
 import com.example.kueat.ui.home.HomeFragment
 import com.example.kueat.ui.like.LikeFragment
 import com.google.android.material.navigation.NavigationBarView
@@ -20,6 +24,8 @@ import com.google.android.material.navigation.NavigationBarView
 class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
     lateinit var binding: ActivityMainBinding
+    var Loc=""
+    var Menu=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,6 +34,11 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         val callback = onBackPressedDispatcher.addCallback(this) {
             backPressed()
         }
+
+        Loc= getIntent().getStringExtra("Loc").toString()
+        Menu= getIntent().getStringExtra("Menu").toString()
+
+
         init()
     }
     private fun init(){
@@ -37,13 +48,25 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
             }
             //초기 화면 홈으로 설정
             selectedItemId = R.id.navigation_home
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.main_frm)
         }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+
         when(item.itemId){
             R.id.navigation_like -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, LikeFragment()).commit()
-            R.id.navigation_home -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commit()
+            R.id.navigation_home -> {
+                    val bundle = Bundle()
+                    bundle.putString("Loc", Loc)
+                    bundle.putString("Menu", Menu)
+                    val homeFragment = HomeFragment()
+                    homeFragment.arguments = bundle
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frm, homeFragment).commit()
+
+                //supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commit()
+            }
             R.id.navigation_appeal -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, AppealFragment()).commit()
             R.id.navigation_account -> supportFragmentManager.beginTransaction().replace(R.id.main_frm, AccountFragment()).commit()
         }
@@ -61,6 +84,5 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
                 }
             }
         }
-
     }
 }
--- app/src/main/java/com/example/kueat/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kueat/ui/home/HomeFragment.kt
@@ -1,15 +1,30 @@
 package com.example.kueat.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.kueat.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
+    var Loc=""
+    var Menu=""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            Loc= it.getString("Loc").toString()
+            Menu= it.getString("Menu")!!
+        }
+
+        //Toast.makeText(requireContext(), Loc +"나오나나오나??"+Menu,Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +32,10 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.fragmentHomeMenu.text=Menu
+        binding.fragmentHomeLocation.text=Loc
+
         return binding.root
     }
 }
