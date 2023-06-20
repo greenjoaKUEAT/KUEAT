fd407cd148a5436ba1d847888225ce36dde6b6cd
youngjunkim <kyj0703@konkuk.ac.kr>
2023-06-20 20:07:50 +0900

    shared

--- app/src/main/java/com/example/kueat/ui/filter/FilterMenuFragment.kt
+++ app/src/main/java/com/example/kueat/ui/filter/FilterMenuFragment.kt
@@ -1,6 +1,7 @@
 package com.example.kueat.ui.filter
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -18,74 +19,62 @@ import com.example.kueat.MainActivity
 import com.example.kueat.R
 import com.example.kueat.databinding.FragmentFilterLocBinding
 import com.example.kueat.databinding.FragmentFilterMenuBinding
+import com.example.kueat.sharedpreference.MyTag
 import com.example.kueat.ui.like.LikeFragment
 
 
 class FilterMenuFragment : Fragment() {
     lateinit var binding: FragmentFilterMenuBinding
-    var Loc = ""
     var Menu = ""
-
+    lateinit var sharedPref:MyTag
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentFilterMenuBinding.inflate(inflater, container, false)
-
-
-        arguments?.let {
-            Loc = it.getString("Loc").toString()
-        }
-
-        Log.d("HI", Loc)
-
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sharedPref = context?.let { MyTag(it) }!!
         initBtn()
     }
 
     private fun initBtn() {
         binding.apply {
-
             all.setOnClickListener {
-                Menu="\"다 좋아\""
+                Menu="다 좋아"
                 changeActivity()
             }
             korea.setOnClickListener {
-                Menu="\"한식\""
+                Menu="한식"
                 changeActivity()
             }
             china.setOnClickListener {
-                Menu="\"중식\""
+                Menu="중식"
                 changeActivity()
             }
             japan.setOnClickListener {
-                Menu="\"일식\""
+                Menu="일식"
                 changeActivity()
             }
             usa.setOnClickListener {
-                Menu="\"양식\""
+                Menu="양식"
                 changeActivity()
             }
             cafe.setOnClickListener {
-                Menu="\"카페\""
+                Menu="카페"
                 changeActivity()
             }
-
         }
     }
 
     fun changeActivity() {
+        context?.let { sharedPref.saveMyMenu(it,Menu) }
+
         val intent = Intent(requireActivity(), MainActivity::class.java)
-
-        intent.putExtra("Loc", Loc)
-        intent.putExtra("Menu", Menu)
-
         requireActivity().startActivity(intent)
 
     }
